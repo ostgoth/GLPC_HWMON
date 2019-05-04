83950b7ff23f3f4bb21ba4a41bd0078a801f5745
ostgoth <andriybanit@gmail.com>
2019-05-04 12:28:47 +0300

    Add CLI args parser

--- hwmon.py
+++ hwmon.py
@@ -1,5 +1,13 @@
 import psutil
+import configargparse
 
-print(psutil.cpu_times())
-print(psutil.virtual_memory(), psutil.swap_memory())
+p = configargparse.ArgParser()
+p.add_argument('-c', '--cpu', action='store_true', help='CPU metrics')
+p.add_argument('-m', '--mem', action='store_true', help='memory metrics')
+args = p.parse_args()
+
+if args.cpu:
+    print(psutil.cpu_times())
+if args.mem:
+    print(psutil.virtual_memory(), psutil.swap_memory())
 
