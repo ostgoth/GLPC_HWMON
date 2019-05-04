5330ed8456d5410686c080793635fa93a85fa065
ostgoth <andriybanit@gmail.com>
2019-05-04 12:32:23 +0300

    Add check if arguments present

--- hwmon.py
+++ hwmon.py
@@ -1,6 +1,15 @@
 import psutil
 import configargparse
 
+
+def check_if_empty_arg_list(namespace):
+    dct = vars(namespace)
+    for key in dct:
+        if dct[key]:
+            break
+    print('Script should be run with arguments. Use --help to get argument list')
+
+
 p = configargparse.ArgParser()
 p.add_argument('-c', '--cpu', action='store_true', help='CPU metrics')
 p.add_argument('-m', '--mem', action='store_true', help='memory metrics')
@@ -10,4 +19,6 @@ if args.cpu:
     print(psutil.cpu_times())
 if args.mem:
     print(psutil.virtual_memory(), psutil.swap_memory())
+
+check_if_empty_arg_list(args)
 
